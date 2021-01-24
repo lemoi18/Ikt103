65913e716d0a4472d7de62cf0b4e248322ce06b0
lemoi18 <lemoi18@uia.no>
2021-01-24 02:13:10 +0100

    Deliverd assignment 1

--- assignment_1_4/main.py
+++ assignment_1_4/main.py
@@ -1,5 +1,15 @@
+word = input()
 
-s = input()
-for i in range(len(s)):
-    b = s.count(s[i])
-    print("'{}' : {}".format(s[i], b))
+Alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+hits = [
+    (Alphabet[i], word.count(Alphabet[i]))
+    for i in range(26)
+
+]
+
+for letter, frequency in hits:
+    print("'%s' : %s" % (letter.lower(), frequency))
